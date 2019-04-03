8715e8374f78893a70ac0cbb136b9e24ccf0303d
pipo17 <49249088+pipo17@users.noreply.github.com>
2019-04-03 20:27:08 +0200

    Dodavanje pocetne skripte

---
+++ pocetni algortiam.py
@@ -0,0 +1,63 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = input (' kako se zoveš')
+ kako se zoveša = input (' kako se zoveš')
+>>> print=a
+>>> print('Ja se zovem'',a,''.)
+      
+SyntaxError: EOL while scanning string literal
+>>> a = input (' kako se zoveš')
+ kako se zoveš
+>>> a = input (' kako se zoveš')
+ kako se zoveš
+>>> a = input (' kako se zoveš') print('Ja se zovem'',a,''.)
+				   
+SyntaxError: invalid syntax
+>>> 
+=============== RESTART: C:/Users/Digitalni Ninja/Desktop/a.py ===============
+kako se zoveš j
+Ja se zovem,a,.
+>>> 
+=============== RESTART: C:/Users/Digitalni Ninja/Desktop/a.py ===============
+kako se zoveš h
+>>> 
+>>> f
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    f
+NameError: name 'f' is not defined
+>>> 
+=============== RESTART: C:/Users/Digitalni Ninja/Desktop/a.py ===============
+kako se zoveš k
+>>> 
+>>> 
+>>> 
+>>> 
+
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> l
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    l
+NameError: name 'l' is not defined
+>>> 
+=============== RESTART: C:/Users/Digitalni Ninja/Desktop/a.py ===============
+kako se zoveš k
+Ja se zovem  k .
+>>> 
